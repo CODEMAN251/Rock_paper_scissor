7fbc2cc00622eb4507b67d60c375b4730c0d51ef
Sai Manjunath R Walke <117581145+CODEMAN251@users.noreply.github.com>
2023-09-14 14:44:10 +0530

    Update RockPaperSci.c
    Added the feature of score and level.
    Added the feature of streak bonus.

--- RockPaperSci.c
+++ RockPaperSci.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Function to generate a random choice for the computer
+#define MAX_GAMES 100
+#define LEVEL_UP_WINS 3
+#define STREAK_BONUS 20 
+
+int currentStreak = 0;
+int streakBonus = 5;    // Example bonus for each consecutive win.
+int score = 0;          // Global score variable
+int level = 1;          // Starting level
+int playerStreak = 0;   // Track the player's winning streak
+
 int getComputerChoice() {
     return rand() % 3;
 }
 
-// Function to display the choices
 void printChoices() {
     printf("\t\t\tPlease choose one of the following options:\n");
     printf("\t\t\t0. Rock\n");
@@ -15,7 +23,6 @@ void printChoices() {
     printf("\t\t\t2. Scissors\n");
 }
 
-// Function to determine the winner of the game
 int determineWinner(int player, int computer) {
     if (player == computer)
         return 0; // Draw
@@ -24,7 +31,7 @@ int determineWinner(int player, int computer) {
     return -1; // Computer wins
 }
 
-void saveGameResults(int results[], int count) {
+void saveGameResults(int results[], int level[], int score[], int streaks[], int count) {
     FILE *file = fopen("game_results.txt", "w");
     if (file == NULL) {
         printf("Error opening the file for writing.\n");
@@ -32,7 +39,8 @@ void saveGameResults(int results[], int count) {
     }
 
     for (int i = 0; i < count; i++) {
-        fprintf(file, "Round %d: ", i + 1);
+        fprintf(file, "Round %d (Level: %d, Score: %d): ", i + 1, level[i], score[i]);
+        
         if (results[i] == 0) {
             fprintf(file, "Draw\n");
         } else if (results[i] == 1) {
@@ -48,14 +56,20 @@ void saveGameResults(int results[], int count) {
 int RockPaperScissorsGame() {
     int computer, player;
     int result;
-    int gameResults[100]; // Assuming a maximum of 100 games
+    int gameResults[MAX_GAMES];
+    int gameLevels[MAX_GAMES];
+    int gameScores[MAX_GAMES];
+    int gameStreaks[MAX_GAMES];
+    int winCount = 0;
+
 
     srand(time(NULL)); // Seed the random number generator
 
     int gameCount = 0;
 
     while (1) {
-        printf("\n\t\t\tWelcome to Rock Paper Scissors Game\n\n\n");
+        printf("\n\t\t\tWelcome to Rock Paper Scissors Game\n");
+        printf("\t\t\tLevel: %d\tScore: %d\n\n", level, score);
         printChoices();
 
         printf("\t\t\tYour Choice: ");
@@ -66,29 +80,62 @@ int RockPaperScissorsGame() {
         }
 
         computer = getComputerChoice();
-
         printf("\t\t\tComputer's choice is: %s\n", (computer == 0) ? "Rock" : (computer == 1) ? "Paper" : "Scissors");
 
         result = determineWinner(player, computer);
 
-        if (result == 0)
+        if (result == 0) 
+        {
             printf("\n\t\t\tIt's a draw!\n");
-        else if (result == 1)
+            score += 1;
+            playerStreak = 0; // Reset streak on draw
+        } 
+        else if (result == 1) 
+        {
             printf("\n\t\t\tYou win!\n");
-        else
-            printf("\n\t\t\tComputer wins!\n");
+            score += 10;
+            winCount++;
+            playerStreak++; // Increment streak on player win
 
+        // Check for streak bonus
+            if (playerStreak == 3) 
+            {
+                printf("\n\t\t\tStreak Bonus! +20 Points\n");
+                score += STREAK_BONUS;
+                playerStreak = 0; // Reset streak after awarding bonus
+            }
+        } 
+        else 
+        {
+            printf("\n\t\t\tComputer wins!\n");
+            score -= 5;
+            playerStreak = 0; // Reset streak on loss
+        } 
+    
+
+        gameLevels[gameCount] = level;
+        gameScores[gameCount] = score;
         gameResults[gameCount] = result;
+        gameStreaks[gameCount] = currentStreak;
         gameCount++;
+        
+        // Level up condition
+        if (winCount == LEVEL_UP_WINS) {
+            level++;
+            printf("\n\t\t\tCongratulations! You've moved to level %d.\n", level);
+            winCount = 0; // Reset win count for next level
+        }
 
         char ch;
         printf("\n\t\t\tDo you want to play again (y/n): ");
         scanf(" %c", &ch);
-        if (ch != 'y' && ch != 'Y')
+        if (ch != 'y' && ch != 'Y') {
+            printf("\n\t\t\tYour final score: %d\n", score);
             break;
+        }
     }
 
-    saveGameResults(gameResults, gameCount);
+    saveGameResults(gameResults, gameLevels, gameScores, gameStreaks, gameCount);
 
     return 0;
 }
